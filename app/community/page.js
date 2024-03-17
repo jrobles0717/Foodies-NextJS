b6a34be58edee34bf41924fd4414831c0e8cb4ee
jrobles0717 <j_robles0717@yahoo.com>
2024-03-17 09:30:56 -0400

    first commit

---
+++ app/community/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function CommunityPage() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>Community Page</h1>
+    </main>
+  );
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function SharePage() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>Meals Share Page</h1>
+    </main>
+  );
+}
